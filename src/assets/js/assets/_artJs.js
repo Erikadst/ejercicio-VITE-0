9354a4e4777da3c91f67b46ebe6b9ba89c180de3
Erikadst <erikadsan@gmail.com>
2026-06-15 14:03:11 +0200

    boton y desplegable

--- src/assets/js/assets/_artJs.js
+++ src/assets/js/assets/_artJs.js
@@ -30,25 +30,42 @@ boton.addEventListener("click", function(){
 
 /* si tengo muchos botones va a cambiar el color al que se haga en click */
 
-const botones = document.querySelectorAll('.boton')
+/* const botones = document.querySelectorAll('.boton')
 
 botones.forEach(function(boton){
     boton.addEventListener('click', function(){
         boton.style.backgroundColor = 'pink'
         boton.style.color = 'black'
     })
-})
+}) */
 
 
 // Funcion flecha 
-/*
+
 const botones = document.querySelectorAll('.boton')
 
 botones.forEach ((boton)=>{
     boton.addEventListener('click', ()=>{
+        //recorro todos los botoner de clase boton para resetearles el color
+        botones.forEach((boton)=>{
+            boton.style.backgroundColor="#00c7a9"
+            boton.style.color="#0e3e90"
+        })
+        //una vez reseteados los botones, cambio el color sobre el que se ha hecho click
         boton.style.backgroundColor='pink'
         boton.style.color='black'
     })
-}) */
+})
 
-//
+//Desplegable
+const botonesDesplegar = document.querySelectorAll(".botonDesplegar")
+
+botonesDesplegar.forEach ((botonDesplegar)=>{
+    botonDesplegar.addEventListener("click", ()=>{
+        //coge el elemento anterior a el previousElementSibling
+        const miParrafo=botonDesplegar.previousElementSibling
+        //classlist para llamar a los estilos y añadir o quitar la clase open lo hace toggle
+        miParrafo.classList.toggle("open")
+    })
+})
+
