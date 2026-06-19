ba5755cb906fbe51f7deedc61cef28dc08559b68
Erikadst <erikadsan@gmail.com>
2026-06-19 14:00:56 +0200

    slider con puntitos

--- src/assets/js/assets/_artSlider.js
+++ src/assets/js/assets/_artSlider.js
@@ -5,6 +5,9 @@ export default function artSlider() {
         $carrousel.forEach(($el) => {
             const $pista = $el.querySelector(".artSlider01__pista");
             const $sliders = $el.querySelectorAll(".artSlider01__slide");
+            const $dots = $el.querySelectorAll(".artSlider01__track__dot");
+
+            console.log($dots);
             //Comprobar que nuestra pista y los slider existan dentro del DOM
             if (!$pista | $sliders.length === 0) return;
             //console.log ($pista, $sliders);
@@ -15,15 +18,38 @@ export default function artSlider() {
             const tiempoEspera = 3000;
 
             $sliders.forEach($slide => {
-                console.log($slide);
-            })
+                const $copia = $slide.cloneNode(true);
+                $pista.appendChild($copia);
+            });
 
             setInterval(() => {
-                console.log(`Contador: ${contador}`);
-                console.log(`Tiempo:${(tiempoEspera * contador)/1000}s`);
                 //contador = contador +1;
                 contador++;
-            }, tiempoEspera);
+                $pista.style.transform= `translateX(-${100*contador}%)`;
+                $pista.style.transition=`transform ${tiempoTransicion}ms`
+                $dots[contador - 1]?.classList.remove("active");
+                $dots[contador]?.classList.add("active");
+                //Siguiente elemento dot
+                if ($dots[contador]){
+                    $dots[contador].classList.add("Active");
+                } else {
+                    $dots[0].classList.add("active");
+                }
+
+                //Validar que el contador sea igual al numero de la ultima posicion del slider
+                if (contador === $sliders.length){
+                    setTimeout(()=>{
+                    //console.log({contador});
+                    //reiniciar contador
+                        contador = 0
+                        $pista.style.transform =`translateX(0)`;
+                        $pista.style.transition="none";
+                    },tiempoTransicion);
+
+                }}, tiempoEspera);
+
+/*          setTimeout(()=> {},tiempoEspera);
+            console.log(`Me ejecuto cada ${tiempoEspera}`) */
 
         })
 }
